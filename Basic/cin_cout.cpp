22682267753fa57008a1359657c16e3fd0467efe
Ansh Parmar <anshparmar8030@gmail.com>
2026-08-10 21:29:16 +0530

    The 1st c++ code

---
+++ Basic/cin_cout.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    cout<<"Enter the value of A : ";
+    cin>> a;
+    cout<<"Enter the value of A : ";
+    cin>> b;
+    return 0;
